5ace07f9cdfe2745d1530a6d30c4409b34c48acf
Manh Cuong Tran <45611820+cuongtm95@users.noreply.github.com>
2024-06-05 15:00:55 +0200

    fix removebloat.ps1 raw url

--- debloat-intune-script.ps1
+++ debloat-intune-script.ps1
@@ -12,7 +12,7 @@ Else {
 $templateFilePath = "C:\ProgramData\Debloat\removebloat.ps1"
 
 Invoke-WebRequest `
--Uri "https://github.com/HPC-Germany/Debloat/raw/main/RemoveBloat.ps1" `
+-Uri "https://raw.githubusercontent.com/HPC-Germany/Debloat/main/RemoveBloat.ps1" `
 -OutFile $templateFilePath `
 -UseBasicParsing `
 -Headers @{"Cache-Control"="no-cache"}
